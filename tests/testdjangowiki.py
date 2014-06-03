4ad6587664c31a85f2777cb260a90574cada4d8b
george <rpubaddr0@gmail.com>
2014-06-03 16:59:42 -0600

    Add initial files for unit testing.

---
+++ tests/testdjangowiki.py
@@ -0,0 +1,10 @@
+import unittest
+try:
+    from mock import Mock
+except ImportError:
+    from unittest.mock import Mock
+
+class TestDjangoWiki(unittest.TestCase):
+
+    def setUp(self):
+        pass
